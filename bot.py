42299aec9f4f5fddefd87a1af81f35437d2f5e09
rainbowinternationalschool <66720450+Meme25327@users.noreply.github.com>
2021-12-25 19:19:30 +0300

    w

--- bot.py
+++ bot.py
@@ -10,6 +10,54 @@ from youtubesearchpython import SearchVideos, Search
 from youtubesearchpython.internal.constants import ResultMode
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
+import json
+import youtube_dl
+import asyncio
+from discord.ext import commands
+import datetime
+import wikipediaapi
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -39,26 +87,30 @@ async def on_message(msg):
     if isinstance(msg.channel, discord.channel.DMChannel):
 
         #stores confessions in a seperate text document
-        #with open("confessions.txt", "r+") as file:
-        #   count = len(file.readlines())
-        #    
-        #   print(confession)
-        #
-        #  file.write(confession + "\n")
-        #   file.close()
         rawMsg = str(msg.content)
         confession = rawMsg.replace('\n', '. ').replace(' .', '.').replace('.. ', '. ')
+        with open("confessions.txt", "a+") as file:
+            count = len(file.readlines())
+            
+            print(confession)
+        
+            file.write(confession + "\n")
+            file.close()
         print("Confession Received")
-        approvalChannel = client.get_channel(772794603954110466) #should end with 0466 when running
+        approvalChannel = client.get_channel(908646167146012716) #should end with 2716 when running
         fullConfession = confession
-        confessionChannel = client.get_channel(772794910826430494) #should end with 0494 when running
+        #confessionChannel = client.get_channel(740256233403056158) #should end with 0494 when running
         if "<@" in msg.content:
             await msg.channel.send("You can't ping anyone in a confession. If you're calling out someone specific, you can still use their name, just not ping them")
         else:
-            if msg.attachments or "http://cdn.discordapp" in msg.content:
-                await msg.channel.send("Sorry, but confessing direct attachments is temporarily not available.")
-            else:
-                await confessionChannel.send('Confession Received: ' + ''.join(fullConfession))
+            await approvalChannel.send('Message Received: ' + ''.join(fullConfession))
+
+        '''if msg.content.startswith("=a") and msg.channel.id == 772780671634374677:#should end with 0466 when running
+            with open("confessions.txt", 'r') as file:
+                lines = file.readlines()
+                num = args[0]
+                print(lines[-num])
+                await confessionChannel.send(lines[-num])'''
 
     #Checks whether the message starts with the prefix. If this is not there, bot replies to every message
     if msg.content.startswith("="):
@@ -69,15 +121,12 @@ async def on_message(msg):
         elif msg.content.startswith('=sunglasses'): #sunglasses command
             if msg.author.id == 258582004738555904:
                 await msg.channel.send("<@258582004738555904> is SO FUCKING COOL. All the ladies fall for him wherever he goes. He is super cool and super smart and super amazing and is the perfect specimen of human being. I really fucking love him because he is so cool and he also made me so that makes him EXTRA COOL!!!!!!!!!!!!!!!!!")
-            elif msg.author.id == 357576484543660043:
-                await msg.channel.send("<@357576484543660043> is down bad.")
+            elif msg.author.id == 445130486491119627:
+                await msg.channel.send("<@445130486491119627> is better than u.")
+            elif msg.author.id == 187160540995387392:
+                await msg.channel.send("B)")
             else:
                 await msg.channel.send(":sunglasses: im really cool, and so is <@" + str(msg.author.id) + ">")
-        elif msg.content.startswith('=speak'): #speak command
-            if "<@" in msg.content:
-                await msg.channel.send("You can't ping anyone with this command.")
-            else:
-                await msg.channel.send(' '.join(args))
         elif msg.content.startswith('=avatar') or msg.content.startswith('=pfp'): #pfp command
             if msg.content == "=pfp" or msg.content == "=avatar" :
                 await msg.channel.send("Your avatar: " + str(msg.author.avatar_url))
@@ -105,19 +154,37 @@ async def on_message(msg):
             #opens the text document from previous code, stores all confessions in a list
             with open('confessions.txt', 'r') as data:
                 data = data.readlines()
-
-            numToApprove = splitMsg.pop(1)
-            print("confession #" + numToApprove, "will be approved.")
+                try:
+                    numToApprove = splitMsg.pop(1)
+                except:
+                    numToApprove = 1
+            if numToApprove == "nsfw" or numToApprove == "spoil":
+                numToApprove = 1
+            print("confession #" + str(numToApprove), "will be approved.")
             confessionChannel = client.get_channel(772794910826430494) #should end with 0494 when running
             
-            if len(args) > 1:
-                if args[1] == "spoiler" or args[1] == "spoil":
-                    await confessionChannel.send("Confession received: || " + data[int(numToApprove)] + " || (spoiler tags typically indicate NSFW content)")
-                else:
-                    approvalChannel = client.get_channel(772794603954110466) #should end with 0466 when running
-                    await approvalChannel.send("Please try that again!")
+            if len(args) > 0:
+                print("ge")
+                if "spoil" in args and "nsfw" in args:
+                    confession = "||" + data[-(int(numToApprove))][:-1] + "|| (spoiler tags typically indicate sensitive content)"
+                    nsfwConfessionChannel = client.get_channel(772794910826430494) #should end with 8120 when running
+                    await nsfwConfessionChannel.send("Message received: " + confession)
+                if "spoil" in args and "nsfw" not in args:
+                    confession = "|| " + data[-(int(numToApprove))][:-1] + " || (spoiler tags typically indicate sensitive content)"
+                    await confessionChannel.send("Message received: " + confession)
+                '''else:
+                    approvalChannel = client.get_channel(772780671634374677) #should end with 0466 when running
+                    await approvalChannel.send("Please try that again!")'''
+                if "nsfw" in args and "spoil" not in args:
+                    nsfwConfessionChannel = client.get_channel(908646804915118120) #should end with 8120 when running
+                    await nsfwConfessionChannel.send("Message received: " + data[-int(numToApprove)])
+                if "nsfw" not in args and "spoil" not in args:
+                    confession = data[-int(numToApprove)]
+                    await confessionChannel.send("Message received: " + confession)
             else:
-                await confessionChannel.send("Confession received: " + data[int(numToApprove)])
+                print("waogj")
+                confession = data[-int(numToApprove)]
+                await confessionChannel.send("Message received: " + confession)
                 
         elif msg.content.startswith('=github'):
             await msg.channel.send("jeuseBot's code can be found at: https://github.com/Meme25327/jeuseBot")
@@ -132,12 +199,13 @@ async def on_message(msg):
             result = str(celsius) + " degrees celsius is equal to " + str(farenheit) + " degrees farenheit"
             await msg.channel.send(''.join(result))
         elif msg.content.startswith('=g') or msg.content.startswith('=google'):
-            query = ''.join(args)
-
+            query = ' '.join(args)
+            
             if len(query) == 0:
                 await msg.channel.send("No query given")
                 return
 
+            await msg.channel.send("Searching for ***" + query + "***. This may take some time, so please be patient.")
             print("google search requested: " + query)
 
             embed = discord.Embed(title = "Search Results")
@@ -145,7 +213,7 @@ async def on_message(msg):
 
             headers = {'User-Agent': 'Mozilla/5.0'}
             
-            for results in search(query, num = 1, stop = 3, pause = 1, tld = "co.in"):
+            for results in search(query, num = 1, stop = 3, pause = 0, tld = "co.in"):
                 print(results)
                 num += 1
                 reqs = requests.get(results)
@@ -165,11 +233,14 @@ async def on_message(msg):
             embed = discord.Embed(title = 'YouTube Results')
             print(info)
             print("--------------------------------------------------------------------------------------------------------------")
-            title = info['result'][0]['title']
-            url = info['result'][0]['link']
-            type = info['result'][0]['type']
-            video = '**', title, '**', ' (', type, ')', ':' '\n', url
-            await msg.channel.send(''.join(video))
+            try:
+                title = info['result'][0]['title']
+                url = info['result'][0]['link']
+                type = info['result'][0]['type']
+                video = '**', title, '**', ' (', type, ')', ':' '\n', url
+                await msg.channel.send(''.join(video))
+            except IndexError:
+                await msg.channel.send("Sorry, no results found. Try refining your query or fixing any typos.")
         elif msg.content.startswith('=nick'):
             num = 0
             guild = msg.guild
@@ -230,7 +301,159 @@ async def on_message(msg):
                 '''
             else:
                 await msg.channel.send("You need to be replying to a message!")
+        elif msg.content.startswith('=define'):
+            word = args[0]
+            headers = {
+                'User-Agent': 'RuneXchange',
+                'From': 'Meme25327#4475'
+            }
+
+            result = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word, headers = headers)
+            resultJson = result.json()
+            num = 0
+            print("="*30)
+            print(word)
+            definition1 = resultJson[0]['meanings'][0]['definitions'][0]['definition']
+            #print(json.dumps(definition, sort_keys=True, indent = 1))
+            embed = discord.Embed(title = "Definition for " + word)
+            embed.add_field(name = "Top Result:", value = definition1, inline = False)
+
+            for definitionNo in resultJson[0]['meanings'][0]['definitions']:
+                wordDef = resultJson[0]['meanings'][0]['definitions'][num]['definition']
+                if num != 0:
+                    embed.add_field(name = "Also see:", value = wordDef, inline = False)
+                num += 1
+            
+            await msg.channel.send(embed = embed)
+        elif msg.content.startswith('=join'):
+            if msg.author.voice:
+                global voiceChannel
+                voiceChannel = msg.author.voice.channel  
+                await voiceChannel.connect()
+                await msg.channel.send("Succesfully joined the voice channel!")
+            else:
+                await msg.channel.send("You need to be in a voice channel to use this command!")
+        elif msg.content.startswith("=play"):
+            try: 
+                print("lol")
+            except:
+                print("unlol")
+        elif msg.content.startswith("=leave"):
+            await self.voiceChannel.disconnect()
+        elif msg.content == "=sex":
+            await msg.channel.send("https://cdn.discordapp.com/attachments/853014263260774400/907336361240043621/image0.gif")
+        elif msg.content.startswith("=jerrry"):
+            await msg.channel.send("Husband. Entrepreneur. Lover. Top 0.7% of OnlyFans creators.")
+        elif msg.content.startswith("=weather"):
+            #--------------------get temps-------------
+            city = ' '.join(args)
+            data = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=f473f2a3e1eb64697114b4dfdbd4e6d5")
+            response = data.json()
+            responseDict = dict(response)
+            try:
+                cityStr = ''.join(responseDict["name"] + ", " + responseDict["sys"]["country"])
+            except KeyError:
+                await msg.channel.send("Couldn't find **" + city + "**. Maybe the city was misspelt, or it isn't in the database.")
+            temps = [responseDict["main"]["temp"]-273.15, responseDict["main"]["feels_like"]-273.15]
+            #results = "The weather in **", city.capitalize(), "** is: **", str(round(temps[0], 2)) + "c**. It feels like: **", str(round(temps[1], 2))+"**c."
+            #------------------end temps------------------
+            #------------------get time---------------------
+            hours = responseDict["timezone"]
+            if hours == 10800:
+                hours = 0
+            else:
+                hours = hours - 10800
+            result = datetime.datetime.now() + datetime.timedelta(seconds=hours)
+            if len(str(result.hour)) == 1:
+                tempStr = "0"
+                resultHour = tempStr + str(result.hour)
+            else:
+                resultHour = str(result.hour)
+            if len(str(result.minute)) == 1:
+                tempStr2 = "0"
+                resultMinute = tempStr2 + str(result.minute)
+            else:
+                resultMinute = str(result.minute)
+            #-------------------end time-----------------------
+            #-------------------get descriptor---------------
+            usedTemp = int(responseDict["main"]["feels_like"]-273.15)
+        
+            if usedTemp <= 7:
+                descriptor = "Cold"
+            else:
+                if usedTemp <= 15:
+                    descriptor = "Cool"
+                else:
+                    if usedTemp <= 23:
+                        descriptor = "Warm"
+                    else:
+                        descriptor = "Hot"
+            #--------------end descriptor------------------
+            #--------------construct embed---------------
+            embed = discord.Embed(title = "Weather for " + cityStr, color = 0xFF5733)
+            iconCode = responseDict["weather"][0]["icon"]
+            embed.set_thumbnail(url = "http://openweathermap.org/img/wn/"+iconCode+"@2x.png")
+            embed.add_field(name = 'Temperature', value = "It is **" + str(round(temps[0], 2)) + "c**.", inline = True)
+            embed.add_field(name = 'Feels Like', value = "It feels like **"+str(round(temps[1], 2))+"c**.", inline = True)
+            embed.add_field(name = 'Time', value = resultHour + ":" + resultMinute, inline = False)
+            weather = responseDict["weather"][0]["main"]
+            embed.add_field(name = "Weather", value = weather, inline = True)
+            embed.add_field(name = 'Description', value = descriptor, inline = True)
+            embed.set_footer(text = "Use fahrenheit? Convert celsius to fahrenheit using the `=ctof [celsius]` command!")
+            #-----------------end embed-------------------
+            await msg.channel.send(embed = embed)
+        elif msg.content.startswith("=jesse"):
+            await msg.channel.send("https://cdn.discordapp.com/attachments/716887644365258772/914135132183601153/unknown.png")
+        elif msg.content.startswith("=sex2014"):
+            await msg.channel.send("https://cdn.discordapp.com/attachments/716887644365258772/917043732195459072/sex2014.gif")
+        elif msg.content.startswith("=wiki") or msg.content.startswith("=wikipedia"):
+            wiki_wiki = wikipediaapi.Wikipedia('en')
+            
+            splitReq = args
+            formattedReq = []
+            
+            for word in splitReq:
+                formattedReq.append(word.capitalize())
+                page_py = wiki_wiki.page('_'.join(formattedReq))
+
+
+            print("="*15, "Wikipedia Search", "="*15)
+            print("Page - Title: %s" % page_py.title)
+            print("Page - Exists: %s" % page_py.exists())
+            print("="*15, "Wikipedia Search", "="*15)
+            
+            summary = []
+            splitSum = page_py.summary.split('. ')
+            print("sentence count:", len(splitSum))
+            i = 0
+            try:
+                while len('. '.join(summary)) < 500:
+                    summary.append(splitSum[i])
+                    i += 1
+                summary.append('')
+            except IndexError:
+                pass
+            print("character count:", len(str(summary)))
+            print('. '.join(summary))
+            print("="*30)
+            try:
+                url = requests.get("http://en.wikipedia.org/w/api.php?action=query&titles="+' '.join(formattedReq)+"&prop=pageimages&format=json&pithumbsize=500")
+                urlJson = url.json()
+                print(urlJson)
+                pageID = urlJson['query']['pages'].keys()
+                thumbnailURL = urlJson['query']['pages'][list(pageID)[0]]['thumbnail']['source']
+            except KeyError:
+                thumbnailURL = "https://cdn.discordapp.com/attachments/716887644365258772/919215219228704818/unknown.png"
+            try:
+                embed = discord.Embed(title = "Wikipedia Article for " + ' '.join(args), color = 0xFF5733)
+                embed.set_thumbnail(url = thumbnailURL)
+                embed.add_field(name = 'Summary', value = '. '.join(summary), inline = True)
+                embed.add_field(name = 'Link', value = page_py.canonicalurl, inline = False)
+            except:
+                await msg.channel.send("Couldn't find that on wikipedia. You could try a google search (`=g`).")
+            await msg.channel.send(embed=embed)
         else:
             await msg.channel.send('Command not recognized. Try =help!')
+        
 
 client.run(TOKEN)
